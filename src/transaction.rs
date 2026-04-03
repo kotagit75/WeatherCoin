dab064d6b0d582ab116fe0068447dbeb5a4e29a6
kotagit75 <unity75lets5@gmail.com>
2026-04-03 15:25:38 +0900

    added features to transaction: sign, verify

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,6 +1,10 @@
+use openssl::error::ErrorStack;
 use serde::{Deserialize, Serialize};
 
-use crate::{address::Address, util::signature::Signature};
+use crate::{
+    address::Address,
+    util::{key::SK, signature::Signature},
+};
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct Transaction {
@@ -15,4 +19,55 @@ impl std::fmt::Display for Transaction {
         write!(f, "{} -> {}: {}", self.sender, self.recipient, self.amount)?;
         Ok(())
     }
+}
+
+impl Transaction {
+    pub fn new(sender: Address, recipient: Address, amount: u64, signature: Signature) -> Self {
+        Self {
+            sender,
+            recipient,
+            amount,
+            signature,
+        }
+    }
+    pub fn new_with_creating_signature(
+        sender: Address,
+        recipient: Address,
+        amount: u64,
+        sk: &SK,
+    ) -> Result<Self, ErrorStack> {
+        Ok(Self {
+            sender: sender.clone(),
+            recipient: recipient.clone(),
+            amount,
+            signature: create_transaction_signature(&sender, &recipient, amount, sk)?,
+        })
+    }
+    pub fn verify(&self) -> bool {
+        self.sender.clone().verify(
+            transacction_to_buf_for_signature(&self.sender, &self.recipient, self.amount)
+                .as_slice(),
+            &self.signature,
+        )
+    }
+}
+
+fn transacction_to_buf_for_signature(
+    sender: &Address,
+    recipient: &Address,
+    amount: u64,
+) -> Vec<u8> {
+    format!("{} -> {}: {}", sender, recipient, amount)
+        .as_bytes()
+        .to_vec()
+}
+
+fn create_transaction_signature(
+    sender: &Address,
+    recipient: &Address,
+    amount: u64,
+    sk: &SK,
+) -> Result<Signature, ErrorStack> {
+    let data = transacction_to_buf_for_signature(sender, recipient, amount);
+    sk.sign(&data)
 }
