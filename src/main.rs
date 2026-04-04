7afc8e8fe6acacfd9bfd21be4417a04bb751141f
kotagit75 <unity75lets5@gmail.com>
2026-04-04 11:31:15 +0900

    implemented update loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use tokio::sync::mpsc;
+
+use crate::update::{run_effect, update};
+
 pub mod beacon;
 pub mod blockchain;
 pub mod node;
@@ -5,12 +9,21 @@ pub mod state;
 pub mod update;
 pub mod util;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let Ok(sk) = node::load_key() else {
         return;
     };
-    let Ok(state) = state::State::new(sk) else {
+    let Ok(mut state) = state::State::new(sk) else {
         return;
     };
-    println!("address: {:?}", state.address)
+    println!("address: {:?}", state.address);
+
+    let (event_tx, mut event_rx) = mpsc::channel(256);
+    while let Some((new_state, effects)) = event_rx.recv().await.map(|event| update(event, state)) {
+        state = new_state;
+        for effect in effects {
+            run_effect(effect).await;
+        }
+    }
 }
--- src/update.rs
+++ src/update.rs
@@ -10,3 +10,5 @@ pub enum Effect {
 pub fn update(event: Event, state: State) -> (State, Vec<Effect>) {
     (state, vec![Effect::None])
 }
+
+pub async fn run_effect(effect: Effect) {}
