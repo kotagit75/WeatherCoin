3cfd26455851a7eaa7df8d4d2714935755e876b4
kotagit75 <unity75lets5@gmail.com>
2026-04-04 09:49:27 +0900

    added function: Chain::generate_next_block

--- src/blockchain/chain.rs
+++ src/blockchain/chain.rs
@@ -1,6 +1,14 @@
+use openssl::error::ErrorStack;
 use serde::{Deserialize, Serialize};
 
-use crate::blockchain::block::{Block, genesis_block};
+use crate::{
+    beacon::Beacon,
+    blockchain::{
+        block::{Block, genesis_block},
+        transaction::Transaction,
+    },
+    util::key::SK,
+};
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 struct Chain {
@@ -12,5 +20,33 @@ impl Chain {
         Self {
             blocks: vec![genesis_block()],
         }
+    }
+
+    pub fn get_latest_block(&self) -> Block {
+        match self.blocks.last() {
+            Some(block) => block.clone(),
+            None => genesis_block(),
+        }
+    }
+
+    pub fn generate_next_block(
+        &self,
+        sk: &SK,
+        beacon: Beacon,
+        transactions: Vec<Transaction>,
+    ) -> Result<Block, ErrorStack> {
+        let previous_block: Block = self.get_latest_block();
+        let next_index: u64 = previous_block.index + 1;
+        let next_timestamp: i64 = chrono::Utc::now().timestamp_millis();
+        Block::new_with_creating_signature(
+            next_index,
+            next_timestamp,
+            transactions,
+            beacon,
+            vec![],
+            &previous_block.issuer,
+            previous_block.hash,
+            sk,
+        )
     }
 }
