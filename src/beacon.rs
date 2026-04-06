7961a6f7a816a398d4d9dcac19c557c8292c036e
kotagit75 <unity75lets5@gmail.com>
2026-04-06 09:22:02 +0900

    [feature] Retrieve temperatures from multiple locations and set the beacon value

--- src/beacon.rs
+++ src/beacon.rs
@@ -7,16 +7,26 @@ pub struct Beacon {
     pub value: i32,
 }
 
-const BEACON_COMMAND: &str = "./beacon.sh";
-
-pub fn get_beacon() -> Option<Beacon> {
+fn get_weather(lat: f64, lon: f64) -> Option<i32> {
     match current_dir() {
-        Ok(x) => Command::new(BEACON_COMMAND)
-            .current_dir(x)
-            .status()
-            .ok()
-            .and_then(|status| status.code()),
+        Ok(x) => {
+            let status = Command::new("./beacon.sh")
+                .arg(lat.to_string())
+                .arg(lon.to_string())
+                .current_dir(x)
+                .status();
+            status.ok().and_then(|status| status.code())
+        }
         Err(_) => None,
     }
-    .map(|value| Beacon { value })
+}
+
+pub fn get_beacon() -> Option<Beacon> {
+    let positions = [(40.782514, -73.965446), (36.2048, 138.2529)];
+    let sum: i32 = positions
+        .map(|pos| get_weather(pos.0, pos.1))
+        .iter()
+        .flatten()
+        .sum();
+    Some(Beacon { value: sum })
 }
