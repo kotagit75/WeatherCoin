c77c3528fd6174f540ed19792326e7a6ef5bbc42
kotagit75 <unity75lets5@gmail.com>
2026-05-10 08:50:01 +0900

    Removed duplicate VDF creation processes

--- src/util/vdf.rs
+++ src/util/vdf.rs
@@ -1,14 +1,18 @@
-use vdf::{PietrzakVDFParams, VDF, VDFParams};
+use vdf::{PietrzakVDF, PietrzakVDFParams, VDF, VDFParams};
 
 const VDF_DIFFICULTY: u64 = 100000;
 const VDF_BITS: u16 = 1024;
 
+fn create_vdf() -> PietrzakVDF {
+    PietrzakVDFParams(VDF_BITS).new()
+}
+
 pub fn verify_solution(challenge: &[u8], solution: &[u8]) -> bool {
-    let vdf = PietrzakVDFParams(VDF_BITS).new();
-    vdf.verify(challenge, VDF_DIFFICULTY, solution).is_ok()
+    create_vdf()
+        .verify(challenge, VDF_DIFFICULTY, solution)
+        .is_ok()
 }
 
 pub fn solve(challenge: &[u8]) -> Result<Vec<u8>, vdf::InvalidIterations> {
-    let vdf = PietrzakVDFParams(VDF_BITS).new();
-    vdf.solve(challenge, VDF_DIFFICULTY)
+    create_vdf().solve(challenge, VDF_DIFFICULTY)
 }
