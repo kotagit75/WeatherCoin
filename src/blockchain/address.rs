bbcf3dec88d793845f5c30f99d7b18d34716a442
kotagit75 <unity75lets5@gmail.com>
2026-05-10 08:47:48 +0900

    changed so that the regex is compiled only once at startup

--- src/blockchain/address.rs
+++ src/blockchain/address.rs
@@ -1,12 +1,12 @@
 use regex::Regex;
+use std::sync::LazyLock;
 
 use crate::util::key::PK;
 
 pub type Address = PK;
 
+static ADDRESS_RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"^[a-fA-F0-9]+$").unwrap());
+
 pub fn is_valid_address(address: &Address) -> bool {
-    let Ok(re) = Regex::new(r"^[a-fA-F0-9]+$") else {
-        return false;
-    };
-    address.der.starts_with("30") && re.is_match(&address.der)
+    address.der.starts_with("30") && ADDRESS_RE.is_match(&address.der)
 }
